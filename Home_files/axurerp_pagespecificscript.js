59aabfd761c259cff0d7942c199f5c6e66188de1
Rebecca Hathaway <rwhathaway@me.com>
2013-05-05 20:00:34 -0700

    axure prototype, v1

---
+++ Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,628 @@
+﻿for(var i = 0; i < 100; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+if (true) {
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+}
+
+});
+
+widgetIdToPanelStateChangeFunction['u99'] = function() {
+var e = windowEvent;
+
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'あ'))) {
+
+	SetPanelState('u7', 'pd1u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'い'))) {
+
+	SetPanelState('u7', 'pd2u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'う'))) {
+
+	SetPanelState('u7', 'pd3u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'え'))) {
+
+	SetPanelState('u7', 'pd4u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'お'))) {
+
+	SetPanelState('u7', 'pd5u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'か'))) {
+
+	SetPanelState('u7', 'pd6u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'き'))) {
+
+	SetPanelState('u7', 'pd7u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'く'))) {
+
+	SetPanelState('u7', 'pd8u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'け'))) {
+
+	SetPanelState('u7', 'pd9u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'こ'))) {
+
+	SetPanelState('u7', 'pd10u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'さ'))) {
+
+	SetPanelState('u7', 'pd11u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'し'))) {
+
+	SetPanelState('u7', 'pd12u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'す'))) {
+
+	SetPanelState('u7', 'pd13u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'せ'))) {
+
+	SetPanelState('u7', 'pd14u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'そ'))) {
+
+	SetPanelState('u7', 'pd15u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'た'))) {
+
+	SetPanelState('u7', 'pd16u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ち'))) {
+
+	SetPanelState('u7', 'pd17u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'つ'))) {
+
+	SetPanelState('u7', 'pd18u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'て'))) {
+
+	SetPanelState('u7', 'pd19u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'と'))) {
+
+	SetPanelState('u7', 'pd20u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'な'))) {
+
+	SetPanelState('u7', 'pd21u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'に'))) {
+
+	SetPanelState('u7', 'pd22u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ぬ'))) {
+
+	SetPanelState('u7', 'pd23u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ね'))) {
+
+	SetPanelState('u7', 'pd24u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'の'))) {
+
+	SetPanelState('u7', 'pd25u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'は'))) {
+
+	SetPanelState('u7', 'pd26u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ひ'))) {
+
+	SetPanelState('u7', 'pd27u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ふ'))) {
+
+	SetPanelState('u7', 'pd28u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'へ'))) {
+
+	SetPanelState('u7', 'pd29u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ほ'))) {
+
+	SetPanelState('u7', 'pd30u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ま'))) {
+
+	SetPanelState('u7', 'pd31u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'み'))) {
+
+	SetPanelState('u7', 'pd32u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'む'))) {
+
+	SetPanelState('u7', 'pd33u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'め'))) {
+
+	SetPanelState('u7', 'pd34u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'も'))) {
+
+	SetPanelState('u7', 'pd35u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'や'))) {
+
+	SetPanelState('u7', 'pd36u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ゆ'))) {
+
+	SetPanelState('u7', 'pd37u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'よ'))) {
+
+	SetPanelState('u7', 'pd38u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ら'))) {
+
+	SetPanelState('u7', 'pd39u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'り'))) {
+
+	SetPanelState('u7', 'pd40u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'る'))) {
+
+	SetPanelState('u7', 'pd41u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'れ'))) {
+
+	SetPanelState('u7', 'pd42u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ろ'))) {
+
+	SetPanelState('u7', 'pd43u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'わ'))) {
+
+	SetPanelState('u7', 'pd44u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'を'))) {
+
+	SetPanelState('u7', 'pd45u7','none','',500,'none','',500);
+
+	SetPanelState('u99', 'pd0u99','none','',500,'none','',500);
+
+	var obj1 = document.getElementById("u1");
+    obj1.focus();
+
+SetWidgetFormText('u1', '');
+
+}
+else
+if (((GetPanelState('u99')) == ('pd1u99')) && (ValueContains(GetWidgetText('u5'), 'ん'))) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('おわり.html');
+
+}
+
+}
+gv_vAlignTable['u80'] = 'top';gv_vAlignTable['u81'] = 'top';gv_vAlignTable['u82'] = 'top';gv_vAlignTable['u83'] = 'top';gv_vAlignTable['u84'] = 'top';gv_vAlignTable['u85'] = 'top';gv_vAlignTable['u86'] = 'top';gv_vAlignTable['u87'] = 'top';gv_vAlignTable['u88'] = 'top';gv_vAlignTable['u89'] = 'top';gv_vAlignTable['u90'] = 'top';gv_vAlignTable['u91'] = 'top';gv_vAlignTable['u92'] = 'top';gv_vAlignTable['u94'] = 'center';gv_vAlignTable['u95'] = 'top';gv_vAlignTable['u96'] = 'top';gv_vAlignTable['u97'] = 'top';gv_vAlignTable['u98'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u22'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u26'] = 'top';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u34'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u36'] = 'top';gv_vAlignTable['u37'] = 'top';gv_vAlignTable['u38'] = 'top';gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u41'] = 'top';gv_vAlignTable['u42'] = 'top';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u0'] = 'top';
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+SetWidgetRichText('u4', '<p style="text-align:left;"><span style="font-family:Arial;font-size:18px;font-weight:normal;font-style:normal;text-decoration:none;color:#000000;">' + (GetWidgetText('u1')) + '</span></p>');
+
+SetWidgetRichText('u6', '<p style="text-align:left;"><span style="font-family:Arial;font-size:9px;font-weight:normal;font-style:normal;text-decoration:none;color:#FFFFFF;">' + (GetWidgetText('u1').length) + '</span></p>');
+
+SetWidgetRichText('u5', '<p style="text-align:left;"><span style="font-family:Arial;font-size:13px;font-weight:normal;font-style:normal;text-decoration:none;color:#FFFFFF;"> ' + (GetWidgetText('u1').substring(GetWidgetText('u1').length - 1, GetWidgetText('u6'))) + '</span></p>');
+
+	SetPanelState('u99', 'pd1u99','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u52'] = 'top';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u56'] = 'top';gv_vAlignTable['u57'] = 'top';gv_vAlignTable['u58'] = 'top';gv_vAlignTable['u59'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u60'] = 'top';gv_vAlignTable['u61'] = 'top';gv_vAlignTable['u62'] = 'top';gv_vAlignTable['u63'] = 'top';gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u66'] = 'top';gv_vAlignTable['u67'] = 'top';gv_vAlignTable['u68'] = 'top';gv_vAlignTable['u69'] = 'top';gv_vAlignTable['u70'] = 'top';gv_vAlignTable['u71'] = 'top';gv_vAlignTable['u72'] = 'top';gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u74'] = 'top';gv_vAlignTable['u75'] = 'top';gv_vAlignTable['u76'] = 'top';gv_vAlignTable['u77'] = 'top';gv_vAlignTable['u78'] = 'top';gv_vAlignTable['u79'] = 'top';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u15'] = 'top';
